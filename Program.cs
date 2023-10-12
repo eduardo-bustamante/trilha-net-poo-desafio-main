7b60ebf068be5b17459ea53d52fb2b9034e0d6e2
Eduardo Bustamante <125780764+eduardo-bustamante@users.noreply.github.com>
2023-10-12 20:49:58 -0300

    Arquivo terminado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "modelo 1", imei: "1234LL", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("");
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "9874", modelo: "iphone 13", imei: "kk25", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Firefox");
